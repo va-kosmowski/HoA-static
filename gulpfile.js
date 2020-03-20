ffe158171c2a45ba43a2b30ff87ff5e14ebec72a
va-kosmowski <krzysztof.kosmowski@valueadd.pl>
2020-03-20 14:35:07 +0100

    Initial commit. Copy of HoA-mockups excluding components. Minor updates added.

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const { watch, src, dest, series, parallel } = require('gulp');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const config = {
+    app: {
+        scss: './app/styles/**/*.scss',
+        html: './app/*.html'
+    },
+    dist: {
+        base: './dist/',
+        fonts: './dist/fonts',
+        images: './dist/images',
+    },
+    extraBundles: [
+        './dist/main.css'
+    ]
+};
+
+function cssTask(done) {
+    src(config.app.scss)
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(rename({ suffix: '.bundle' }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest(config.dist.base));
+    done();
+}
+
+function watchFiles() {
+    watch(config.app.html, series(reload));
+    watch(config.app.scss, series(cssTask, reload));
+}
+
+function liveReload(done) {
+    browserSync.init({
+        server: {
+            baseDir: 'app',
+            routes: {
+                '/dist': 'dist/',
+                '/blog': 'app/blog.html',
+                '/hire': 'app/hire.html',
+                '/knowledgebase': 'app/knowledgebase.html',
+                '/home': 'app/home.html',
+                '/open-source': 'app/open-source.html',
+            }
+        }
+    });
+    done();
+}
+
+function reload (done) {
+    browserSync.reload();
+    done();
+}
+
+exports.dev = parallel(cssTask, watchFiles, liveReload);
+ // run gulp dev to work
